572733e1033669080c1cd0bc6bb86816165bc742
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-01-21 18:38:25 +0100

    Initialize java.xml at run time

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JDKInitializationMigrationFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JDKInitializationMigrationFeature.java
@@ -25,6 +25,10 @@
 package com.oracle.svm.hosted.jdk;
 
 import static com.oracle.svm.core.SubstrateOptions.InitializeJDKAtBuildTimeMigration;
+import static com.oracle.svm.hosted.jdk.JDKInitializationFeature.JDK_CLASS_REASON;
+
+import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport;
 
 import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
 import com.oracle.svm.core.feature.InternalFeature;
@@ -34,10 +38,23 @@ public class JDKInitializationMigrationFeature implements InternalFeature {
 
     @Override
     public void afterRegistration(AfterRegistrationAccess access) {
-        // Checkstyle: stop
+        RuntimeClassInitializationSupport rci = ImageSingletons.lookup(RuntimeClassInitializationSupport.class);
         if (InitializeJDKAtBuildTimeMigration.getValue()) {
             /* Place all excluded entries from JDKInitializationFeature here. */
+            rci.initializeAtBuildTime("jdk.xml", JDK_CLASS_REASON);
+            /*
+             * The XML classes have cyclic class initializer dependencies, and class initialization
+             * can deadlock/fail when initialization is started at the "wrong part" of the cycle.
+             * Force-initializing the correct class of the cycle here, in addition to the
+             * "whole package" initialization above, breaks the cycle because it triggers immediate
+             * initialization here before the static analysis is started.
+             */
+            rci.initializeAtBuildTime("jdk.xml.internal.JdkXmlUtils", JDK_CLASS_REASON);
+            /* XML-related */
+            rci.initializeAtBuildTime("com.sun.xml", JDK_CLASS_REASON);
+            rci.initializeAtBuildTime("com.sun.org.apache", JDK_CLASS_REASON);
+            rci.initializeAtRunTime("com.sun.org.apache.xml.internal.serialize.HTMLdtd", "Fails build-time initialization");
+            rci.initializeAtBuildTime("com.sun.org.slf4j.internal", JDK_CLASS_REASON);
         }
-        // Checkstyle: resume
     }
 }
